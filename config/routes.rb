b5c5996f1c3d360819aa5e70cd3b44b0a3bedb19
Ivan Kusalic <ikusalic@gmail.com>
2012-05-06 19:09:57 +0200

    Add a StaticPages controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Blog::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -51,8 +54,4 @@ Blog::Application.routes.draw do
   # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end

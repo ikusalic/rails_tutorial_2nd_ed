d18c6ab49ef2da29c16dcc1a5fa5b0a6edd2d4f5
Ivan Kusalic <ikusalic@gmail.com>
2012-05-20 12:08:16 +0200

    Implement creation of microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
 
+  def home
+    @micropost = current_user.microposts.build if signed_in?
+  end
+
   def about
   end
 
@@ -7,8 +11,5 @@ class StaticPagesController < ApplicationController
   end
 
   def help
-  end
-
-  def home
   end
 end
